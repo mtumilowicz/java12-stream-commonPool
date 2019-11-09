aa010fb99c65315c41d0e96ceb337b348a4b7d53
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-11-09 16:44:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# java12-stream-commonPool
+
+
+* https://www.youtube.com/watch?v=IwJ-SCfXoAU
+* 54.12 - size of common pool, 55.49 - main is actually a part of it
+* `-Djava.util.concurrent.ForkJoinPool.common.parallelism=100` // try to avoid it
---
+++ src/main/java/X.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+/**
+ * Created by mtumilowicz on 2019-07-30.
+ */
+public class X {
+
+    static int transform(int number) {
+        System.out.println("transforming " + number + " " + Thread.currentThread());
+
+        return number;
+    }
+
+    // check on parallel stream where are executed
+    // it doesn't really matter where you create the stream but where you invoke terminal operation
+    static void process(Stream<Integer> stream) throws InterruptedException {
+        ForkJoinPool fjp = new ForkJoinPool(50);
+
+        fjp.submit(() -> stream.forEach(e -> {}));
+
+        fjp.shutdown();
+
+        fjp.awaitTermination(30, TimeUnit.SECONDS);
+    }
+}
